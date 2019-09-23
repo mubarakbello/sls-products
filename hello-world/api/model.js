0b1f01cf2132c15948c4fc515bc6e8a954ac4d04
Mubarak Bello <bellomuba.rak0@gmail.com>
2019-09-23 12:57:33 +0100

    Integrated ES with mongoosastic plugin

--- hello-world/api/model.js
+++ hello-world/api/model.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
+const mongoosastic = require('mongoosastic');
 
 const Products = mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    es_indexed: true
   },
   price: {
     type: Number,
     required: true
   },
   description: {
-    type: String
+    type: String,
+    es_indexed: true
   },
   sku: {
     type: Number,
@@ -25,4 +28,19 @@ const Products = mongoose.Schema({
   }
 });
 
-module.exports = mongoose.model('Products', Products);
+Products.plugin(mongoosastic, {
+  host: "<ES-hostname-here>",
+  port: "<ES-port-here>Number",
+  protocol: "https",
+  auth: "<ES-auth-details-here>"
+});
+
+const products = mongoose.model('Products', Products);
+
+products.createMapping(err => {
+  if (err) {
+    console.error("Error creating mapping (You can safely ignore this):", err);
+  }
+});
+
+module.exports = products
